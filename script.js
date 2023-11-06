827854d7cf9494a45ce39da8ff4864ad44ffe024
Hazel <hazelchavez56@gmail.com>
2023-11-06 11:04:57 -0600

    Modificando textarea

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
-
-function encriptar(){
-   document.querySelector('.muñeco').style.display = "none";
-    document.querySelector('p').style.display = "none";
-    document.querySelector('.cuadro').style.height = "600px";
-    document.querySelector('.mensaje-text').style.paddingTop = '1rem'
-    var texto = document.querySelector('#area-texto').value;/*texto que ingresa el usuario*/
-    var encriptarTexto = texto.replace(/e/gi,"enter").replace(/i/gi,"imes").replace(/a/gi,"ai").replace(/o/gi,"ober").replace(/u/gi,"ufat");/*claves para encriptar*/
-    document.querySelector('.mensaje-text').value = encriptarTexto;
-    document.querySelector('#area-texto').value;
-    document.querySelector('#area-texto').value = '';
-  }
-
-  // const none = (encriptar) =>{
-  //   if(encriptar === texto ){
-  //     document.querySelector('.muñeco').style.display= 'none';
-  //     document.querySelector('.mensaje-text').style.display= 'none';
-  //   }
-  // }
-  
-
-function desencriptar(){
-    var texto = document.querySelector('#area-texto').value;
-    var desencriptarTexto = texto.replace(/enter/gi,"e").replace(/imes/gi,"i").replace(/ai/gi,"a").replace(/ober/gi,"o").replace(/ufat/gi,"u");
-    document.querySelector('.mensaje-text').value = desencriptarTexto;
-    document.querySelector('#area-texto').value;
-
+function encriptar() {
+	document.querySelector('.muñeco').style.display = 'none';
+	document.querySelector('p').style.display = 'none';
+	document.querySelector('.cuadro').style.height = '600px';
+	document.querySelector('.mensaje-text').style.paddingTop = '1rem';
+	var texto =
+		document.querySelector(
+			'#area-texto'
+		).value; /*texto que ingresa el usuario*/
+	var encriptarTexto = texto
+		.replace(/e/gi, 'enter')
+		.replace(/i/gi, 'imes')
+		.replace(/a/gi, 'ai')
+		.replace(/o/gi, 'ober')
+		.replace(/u/gi, 'ufat'); /*claves para encriptar*/
+	document.querySelector('.mensaje-text').value = encriptarTexto;
+	document.querySelector('#area-texto').value;
+	document.querySelector('#area-texto').value = '';
 }
 
-function copiar(){
-  var text = document.getElementById('mensaje-text').value;
-
-  navigator.clipboard.writeText(text)
-  
-  
-  .then(() => {
-    console.log('texto copiado');
-  })
- .catch(err => {
-   console.log('Error al copiar texto ', err);
- });
+function desencriptar() {
+	var texto = document.querySelector('#area-texto').value;
+	var desencriptarTexto = texto
+		.replace(/enter/gi, 'e')
+		.replace(/imes/gi, 'i')
+		.replace(/ai/gi, 'a')
+		.replace(/ober/gi, 'o')
+		.replace(/ufat/gi, 'u');
+	document.querySelector('.mensaje-text').value = desencriptarTexto;
+	document.querySelector('#area-texto').value;
 }
 
+function copiar() {
+	var text = document.getElementById('mensaje-text').value;
+
+	navigator.clipboard
+		.writeText(text)
+
+		.then(() => {
+			console.log('texto copiado');
+		})
+		.catch((err) => {
+			console.log('Error al copiar texto ', err);
+		});
+}
